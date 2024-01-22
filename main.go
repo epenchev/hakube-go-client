f4d08ef293c4043744fe351f9b0f7ce16a01ead6
Emil Penchev <emil.penchev@suse.com>
2024-01-22 08:54:24 +0200

    Added code for handling unstructured objects.

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	v1beta1 "hakube-go-client/v1beta1"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/client-go/kubernetes/scheme"
 
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -15,9 +18,7 @@ import (
 )
 
 func createOperatorScheme() (*runtime.Scheme, error) {
-	// create a new scheme specifically for this manager
 	hakubeScheme := runtime.NewScheme()
-
 	// add standard resource types to the scheme
 	if err := scheme.AddToScheme(hakubeScheme); err != nil {
 		return nil, err
@@ -42,17 +43,29 @@ func getClient() client.Client {
 	return cl
 }
 
-func main() {
+func getClientNoScheme() client.Client {
+	cfg, err := ctrl.GetConfig()
+	if err != nil {
+		log.Fatalln("Error getting config")
+	}
+	cl, err := client.New(cfg, client.Options{})
+	if err != nil {
+		log.Fatalln("failed to create client")
+	}
+	return cl
+}
+
+func getObjectsWithScheme() {
 	cl := getClient()
 
 	objectName := "saphana-scaleup"
 	namespace := "hakube-manager"
 
 	// Get node attributes storage object.
-	// hkbnattr := &v1beta1.HAKubeNodeAttributes{}
-	//if err := cl.Get(context.Background(), client.ObjectKey{Namespace: namespace, Name: objectName}, hkbnattr); err != nil {
-	//    log.Fatalln("failed to fetch node attributes store object, error:", err)
-	//}
+	hkbnattr := &v1beta1.HAKubeNodeAttributes{}
+	if err := cl.Get(context.Background(), client.ObjectKey{Namespace: namespace, Name: objectName}, hkbnattr); err != nil {
+		log.Fatalln("failed to fetch node attributes store object, error:", err)
+	}
 
 	// Get resource controller object.
 	hactrl := &v1beta1.HAKubeController{}
@@ -61,5 +74,40 @@ func main() {
 	}
 	// Dump objects
 	fmt.Printf("resource configuration %+v\n", hactrl.Spec.Resources)
-	//fmt.Printf("node-attributes %+v\n", hkbnattr.NodeAttributes)
+	fmt.Printf("node-attributes %+v\n", hkbnattr.NodeAttributes)
+}
+
+// see https://mittachaitu.medium.com/dealing-with-unstructured-objects-f190629e7d60
+// see https://pkg.go.dev/k8s.io/apimachinery/pkg/apis/meta/v1/unstructured
+// see https://github.com/kubernetes/apimachinery/blob/master/pkg/apis/meta/v1/unstructured/helpers.go
+// for dealing with unstructured objects
+func getObjectsUnstructured() {
+	cl := getClientNoScheme()
+
+	objectName := "saphana-scaleup"
+	namespace := "hakube-manager"
+
+	// Get resource controller object.
+	kind := "HAKubeController"
+	apiversion := "hakube.io/v1beta1"
+
+	obj := new(unstructured.Unstructured)
+	obj.SetName(objectName)
+	obj.SetAPIVersion(apiversion)
+	obj.SetGroupVersionKind(schema.FromAPIVersionAndKind(apiversion, kind))
+	key := client.ObjectKey{Name: obj.GetName(), Namespace: namespace}
+	if err := cl.Get(context.Background(), key, obj); err != nil {
+		log.Fatalln("failed to retrieve object", obj.GetKind(), key.Namespace, key.Name)
+	}
+	fmt.Printf("object contents %+v\n\n", obj)
+
+	resourceConfig, _, err := unstructured.NestedFieldNoCopy(obj.Object, "spec", "resources")
+	if err == nil {
+		fmt.Printf("resource configuration %+v\n", resourceConfig)
+	}
+}
+
+func main() {
+	getObjectsWithScheme()
+	//getObjectsUnstructured()
 }
